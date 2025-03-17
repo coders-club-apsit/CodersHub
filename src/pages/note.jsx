38ed2730f7340aa235eace6d729b290fd698418f
AvanishVadke <avanishvadke001@gmail.com>
2025-03-17 12:19:48 +0530

    improved better visibility of notePage for light mode system theme

--- src/pages/note.jsx
+++ src/pages/note.jsx
@@ -1,25 +1,24 @@
-import React, { useEffect } from 'react'
-import { useUser } from '@clerk/clerk-react';
-import { useParams } from 'react-router-dom';
-import useFetch from '@/hooks/use-fetch';
-import { getSingleNote } from '@/api/api-Notes';
-import { BarLoader, PropagateLoader } from 'react-spinners';
-import MDEditor from '@uiw/react-md-editor';
-import Header from '@/components/header';
+import React, { useEffect } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { useParams } from "react-router-dom";
+import useFetch from "@/hooks/use-fetch";
+import { getSingleNote } from "@/api/api-Notes";
+import { BarLoader } from "react-spinners";
+import MDEditor from "@uiw/react-md-editor";
+import Header from "@/components/header";
 
 const NotePage = () => {
-
-  const {isLoaded, user} = useUser();
+  const { isLoaded, user } = useUser();
   const { id } = useParams();
 
   const {
     fn: fnNotes,
     data: notes,
     loading: loadingNotes,
-  } = useFetch(getSingleNote, { note_id: id, });
+  } = useFetch(getSingleNote, { note_id: id });
 
   useEffect(() => {
-  if(isLoaded) fnNotes();
+    if (isLoaded) fnNotes();
   }, [isLoaded]);
 
   if (!isLoaded || loadingNotes) {
@@ -28,18 +27,29 @@ const NotePage = () => {
 
   return (
     <>
-    <Header/>
-    <div className='flex flex-col gap-8 mt-9 m-6'>
-      <div className='flex flex-col-reverse gap-6 md:flex-row justify-between items-center'>
-        <h1 className='gradient-title font-extrabold pb-3 text-4xl sm:text-6xl'>{notes?.title}</h1>
-        <img src={notes?.topics?.topic_logo_url} className='h-12' alt={notes?.title}></img>
+      <Header />
+      <div className="flex flex-col gap-8 mt-9 m-6">
+        <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
+          <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
+            {notes?.title}
+          </h1>
+          <img
+            src={notes?.topics?.topic_logo_url}
+            className="h-12"
+            alt={notes?.title}
+          ></img>
+        </div>
+        <h2 className="text-2xl sm:text-3xl font-bold">About this Note</h2>
+        <p className="sm:text-lg">{notes?.description}</p>
+        <div data-color-mode="dark">
+          <MDEditor.Markdown
+            source={notes?.content}
+            className="text-white sm:text-lg"
+          />
+        </div>
       </div>
-      <h2 className='text-2xl sm:text3xl font-bold'>About this Note</h2>
-      <p className='sm:text-lg'>{notes?.description}</p>
-      <MDEditor.Markdown source={notes?.content} className='bg-transparent sm:text-lg' />
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default NotePage
+export default NotePage;

776f6b0bae3152135fc75d6b8b495d9d058b332d
YadneshBamne <yadnesh2105@gmail.com>
2025-06-28 22:16:22 +0530

    chore: update jspdf to version 3.0.1 and add lenis for smooth scrolling
    - Updated jspdf from version 2.5.2 to 3.0.1 in package.json.
    - Added lenis package (version 1.3.4) for smooth scrolling experience.
    - Integrated ReactLenis in main.jsx to enable smooth scrolling functionality.

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import { dark } from '@clerk/themes';
 import { Analytics } from '@vercel/analytics/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {ReactLenis} from "lenis/react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) throw new Error('Missing Publishable Key');
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider appearance={{ baseTheme: dark }} publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <QueryClientProvider client={queryClient}>
+        <ReactLenis root/>
         <App />
         <Analytics />
       </QueryClientProvider>
